e2b5f460a4b35ab8e6d5e51154e775e260f128e5
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2017-08-07 16:08:11 +0200

    Adds two new REST API functionalities; update and delete order.

--- src/main/java/com/example/controllers/RestAPIController.java
+++ src/main/java/com/example/controllers/RestAPIController.java
@@ -25,12 +25,22 @@ public class RestAPIController {
 	}
 
 	@RequestMapping(value = "/api/orders/{customerName}", method = RequestMethod.GET)
-	public List<Order> getOrder(@PathVariable("costumerName") String customerName) {
+	public List<Order> getOrder(@PathVariable("customerName") String customerName) {
 		return repo.findByCostumerName(customerName);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/api/orders")
 	public void addOrder(@RequestBody Order newOrder) {
 		repo.save(newOrder);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/api/orders/{customerName}")
+	public void updateOrder(@RequestBody Order updateOrder, @PathVariable String customerName) {
+		repo.save(updateOrder);
+	}
+
+	@RequestMapping(value = "/api/orders/{customerName}", method = RequestMethod.DELETE)
+	public void deleteOrder(@PathVariable("customerName") long customerName) {
+		repo.deleteOrderById(customerName);
 	}
 }
