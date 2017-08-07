5b2d3ded553004dc9faa0853afc6bbaeb17636f6
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2017-08-07 23:29:08 +0200

    Adds comment to code in FormNotValidatedException.

--- src/main/java/com/example/exceptions/FormNotValidatedexception.java
+++ src/main/java/com/example/exceptions/FormNotValidatedexception.java
@@ -1,8 +1,21 @@
 package com.example.exceptions;
 
+/**
+ * The FormNotValidatedException class extens the Exception class and represents
+ * an exception for when a form is not validated.
+ * 
+ * @author Ramin Esfandiari </br>
+ *         7. aug. 2017 </br>
+ *
+ */
 public class FormNotValidatedexception extends Exception {
 
+	/**
+	 * Constructs a new instance with a given error message.
+	 * 
+	 * @param message
+	 */
 	public FormNotValidatedexception(String message) {
-		super(message);
+		super(message); // Construct the superclass and pass the message as argument.
 	}
 }
